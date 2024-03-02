a62675946d21d1a7e4127032991726a17946b48d
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2024-03-02 17:18:47 +0100

    compilation flags

---
+++ build/output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+int v_a = ( ( 4 + 2 ) * 7 );
+int v_b = 2;
+int v_c = ( v_a + v_b );
+if ( ( v_av_b ) ) {
+int v_r = 0;
+} else {
+}
+return 0;
+}
--- test.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-
-make && ./compiler test.cp 
