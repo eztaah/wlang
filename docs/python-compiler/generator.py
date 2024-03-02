7785486547ac369b89c1c7e4a84f6b88269c4855
eztaah <eztaah@gmail.com>
2024-03-02 09:00:23 +0100

    remove comment block python code

--- docs/python-compiler/generator.py
+++ docs/python-compiler/generator.py
@@ -1,54 +1,6 @@
 from data import NumberNode, BinOpNode, VarDeclNode, VarRefNode, ProgramNode, IfNode
 
 
-# def generate_assembly(node, variables=None, offset=0):
-#     if variables is None:
-#         variables = {}
-    
-#     instructions = []
-
-#     if isinstance(node, ProgramNode):
-#         # Initialisation du cadre de pile et des variables
-#         instructions.append(";rbp est fixe")
-#         instructions.append("push rbp         ; on sauvegarde le registre base pointer")  # Sauvegarde l'ancienne base de la pile
-#         instructions.append("mov rbp, rsp     ; on met le contenu de rsp dans rbp\n")  # Nouvelle base de la pile
-#         for stmt in node.statements:
-#             instructions += generate_assembly(stmt, variables, offset)
-#             offset -= 8  # Supposons que chaque variable occupe 8 octets
-#         instructions.append("\n; end of the program")
-#         instructions.append("mov rsp, rbp")  # Restaure le pointeur de pile
-#         instructions.append("pop rbp")  # Restaure l'ancienne base de la pile
-#         # Fin du programme (ici, nous pourrions ajouter des instructions pour quitter proprement)
-    
-#     elif isinstance(node, VarDeclNode):
-#         var_offset = f"[rbp-{abs(offset)}]"
-#         variables[node.name] = var_offset
-#         instructions += generate_assembly(node.value, variables, offset)
-#         instructions.append(f"mov {var_offset}, rax")  # Stocke le résultat dans la variable
-
-#     elif isinstance(node, NumberNode):
-#         instructions.append(f"mov rax, {node.value}")  # Charge la valeur dans rax
-
-#     elif isinstance(node, BinOpNode):
-#         # Gère l'opérande droit et le stocke dans rax
-#         instructions += generate_assembly(node.right, variables, offset)
-#         instructions.append("push rax")  # Sauvegarde rax sur la pile
-#         # Gère l'opérande gauche, résultat dans rax
-#         instructions += generate_assembly(node.left, variables, offset)
-#         instructions.append("pop rbx")  # Restaure la valeur dans rbx
-#         if node.op == 'PLUS':
-#             instructions.append("add rax, rbx")  # Additionne rax et rbx, stocke le résultat dans rax
-
-#     elif isinstance(node, VarRefNode):
-#         var_offset = variables.get(node.name)
-#         if var_offset is not None:
-#             instructions.append(f"mov rax, {var_offset}")  # Charge la valeur de la variable dans rax
-#         else:
-#             raise ValueError(f"Undefined variable {node.name}")
-
-#     return instructions
-
-
 def generate_assembly(node, variables=None, offset=0, label_count=0):
     if variables is None:
         variables = {}
