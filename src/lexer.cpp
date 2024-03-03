92b1f0f0143e7cceb9e46ec613d42ab5682403b6
eztaah <eztaah@gmail.com>
2024-03-03 21:36:19 +0100

    format

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,4 +1,5 @@
 #include "lexer.hpp"
+
 #include <cctype>
 #include <iostream>
 #include <set>
@@ -6,158 +7,168 @@
 
 std::vector<Token> lexer(const std::string &code)
 {
-  std::vector<Token> tokens;
-  size_t i = 0;
-  std::set<std::string> int_sizes = {"8", "16", "32", "64", "128"};
-  std::set<std::string> float_sizes = {"32", "64"};
+    std::vector<Token> tokens;
+    size_t i = 0;
+    std::set<std::string> int_sizes = {"8", "16", "32", "64", "128"};
+    std::set<std::string> float_sizes = {"32", "64"};
 
-  while (i < code.length()) {
+    while (i < code.length()) {
+        // Handling numbers
+        if (isdigit(code[i])) {
+            std::string num;
+            while (i < code.length() && isdigit(code[i])) {
+                num += code[i];
+                i++;
+            }
+            tokens.push_back(Token(NUMBER, num));
+        }
 
-    // Handling numbers
-    if (isdigit(code[i])) {
-      std::string num;
-      while (i < code.length() && isdigit(code[i])) {
-        num += code[i];
-        i++;
-      }
-      tokens.push_back(Token(NUMBER, num));
+        // Handling operators
+        else if (code[i] == '+') {
+            tokens.push_back(Token(PLUS, ""));
+            i++;
+        }
+        else if (code[i] == '*') {
+            tokens.push_back(Token(TIMES, ""));
+            i++;
+        }
+        else if (code[i] == '-') {
+            tokens.push_back(Token(MINUS, ""));
+            i++;
+        }
+        else if (code[i] == '/') {
+            tokens.push_back(Token(DIVIDE, ""));
+            i++;
+        }
+        else if (code[i] == '=') {
+            if (i + 1 < code.length() && code[i + 1] == '=') {
+                tokens.push_back(Token(EQUALS_EQUALS, ""));
+                i += 2;
+            }
+            else {
+                tokens.push_back(Token(EQUALS, ""));
+                i++;
+            }
+        }
+        else if (code[i] == ';') {
+            tokens.push_back(Token(SEMICOLON, ""));
+            i++;
+        }
+
+        // Handling parentheses and braces
+        else if (code[i] == '(') {
+            tokens.push_back(Token(LPAREN, ""));
+            i++;
+        }
+        else if (code[i] == ')') {
+            tokens.push_back(Token(RPAREN, ""));
+            i++;
+        }
+        else if (code[i] == '{') {
+            tokens.push_back(Token(LBRACE, ""));
+            i++;
+        }
+        else if (code[i] == '}') {
+            tokens.push_back(Token(RBRACE, ""));
+            i++;
+        }
+
+        // Handling variables and keywords
+        else if (isalpha(code[i])) {
+            std::string ident;
+            while (i < code.length() &&
+                   (isalpha(code[i]) || isdigit(code[i]) || code[i] == '_')) {
+                ident += code[i];
+                i++;
+            }
+            // Handling keywords
+            if (ident == "let") {
+                tokens.push_back(Token(LET, ""));
+            }
+            else if (ident == "var") {
+                tokens.push_back(Token(VAR, ""));
+            }
+            else if (ident == "if") {
+                tokens.push_back(Token(IF, ""));
+            }
+
+            // handling var names
+            else {
+                tokens.push_back(Token(IDENTIFIER, ident));
+            }
+        }
+
+        // Ignoring spaces, tabs, new lines
+        else if (code[i] == ' ' || code[i] == '\n' || code[i] == '\t') {
+            i++;
+        }
+        // Error otherwise
+        else {
+            throw std::runtime_error("Unknown character: " + std::string(1, code[i]));
+        }
     }
-
-    // Handling operators
-    else if (code[i] == '+') {
-      tokens.push_back(Token(PLUS, ""));
-      i++;
-    } else if (code[i] == '*') {
-      tokens.push_back(Token(TIMES, ""));
-      i++;
-    } else if (code[i] == '-') {
-      tokens.push_back(Token(MINUS, ""));
-      i++;
-    } else if (code[i] == '/') {
-      tokens.push_back(Token(DIVIDE, ""));
-      i++;
-    } else if (code[i] == '=') {
-      if (i + 1 < code.length() && code[i + 1] == '=') {
-        tokens.push_back(Token(EQUALS_EQUALS, ""));
-        i += 2;
-      } else {
-        tokens.push_back(Token(EQUALS, ""));
-        i++;
-      }
-    } else if (code[i] == ';') {
-      tokens.push_back(Token(SEMICOLON, ""));
-      i++;
-    }
-
-    // Handling parentheses and braces
-    else if (code[i] == '(') {
-      tokens.push_back(Token(LPAREN, ""));
-      i++;
-    } else if (code[i] == ')') {
-      tokens.push_back(Token(RPAREN, ""));
-      i++;
-    } else if (code[i] == '{') {
-      tokens.push_back(Token(LBRACE, ""));
-      i++;
-    } else if (code[i] == '}') {
-      tokens.push_back(Token(RBRACE, ""));
-      i++;
-    }
-
-    // Handling variables and keywords
-    else if (isalpha(code[i])) {
-      std::string ident;
-      while (i < code.length() &&
-             (isalpha(code[i]) || isdigit(code[i]) || code[i] == '_')) {
-        ident += code[i];
-        i++;
-      }
-      // Handling keywords
-      if (ident == "let") {
-        tokens.push_back(Token(LET, ""));
-      } else if (ident == "var") {
-        tokens.push_back(Token(VAR, ""));
-      } else if (ident == "if") {
-        tokens.push_back(Token(IF, ""));
-      }
-
-      // handling var names
-      else {
-        tokens.push_back(Token(IDENTIFIER, ident));
-      }
-    }
-
-    // Ignoring spaces, tabs, new lines
-    else if (code[i] == ' ' || code[i] == '\n' || code[i] == '\t') {
-      i++;
-    }
-    // Error otherwise
-    else {
-      throw std::runtime_error("Unknown character: " + std::string(1, code[i]));
-    }
-  }
-  // Adding end-of-file character
-  tokens.push_back(Token(EOF_TOKEN, ""));
-  return tokens;
+    // Adding end-of-file character
+    tokens.push_back(Token(EOF_TOKEN, ""));
+    return tokens;
 }
 
 std::string tokenTypeToString(TokenType type)
 {
-  switch (type) {
-  case LET:
-    return "LET";
-  case VAR:
-    return "VAR";
-  case NUMBER:
-    return "NUMBER";
-  case PLUS:
-    return "PLUS";
-  case TIMES:
-    return "TIMES";
-  case MINUS:
-    return "MINUS";
-  case DIVIDE:
-    return "DIVIDE";
-  case EQUALS:
-    return "EQUALS";
-  case EQUALS_EQUALS:
-    return "EQUALS_EQUALS";
-  case LPAREN:
-    return "LPAREN";
-  case RPAREN:
-    return "RPAREN";
-  case LBRACE:
-    return "LBRACE";
-  case RBRACE:
-    return "RBRACE";
-  case IF:
-    return "IF";
-  case IDENTIFIER:
-    return "IDENTIFIER";
-  case EOF_TOKEN:
-    return "EOF_TOKEN";
-  case SEMICOLON:
-    return "SEMICOLON";
-  case COMMA:
-    return "COMMA";
-  default:
-    return "UNKNOWN";
-  }
+    switch (type) {
+    case LET:
+        return "LET";
+    case VAR:
+        return "VAR";
+    case NUMBER:
+        return "NUMBER";
+    case PLUS:
+        return "PLUS";
+    case TIMES:
+        return "TIMES";
+    case MINUS:
+        return "MINUS";
+    case DIVIDE:
+        return "DIVIDE";
+    case EQUALS:
+        return "EQUALS";
+    case EQUALS_EQUALS:
+        return "EQUALS_EQUALS";
+    case LPAREN:
+        return "LPAREN";
+    case RPAREN:
+        return "RPAREN";
+    case LBRACE:
+        return "LBRACE";
+    case RBRACE:
+        return "RBRACE";
+    case IF:
+        return "IF";
+    case IDENTIFIER:
+        return "IDENTIFIER";
+    case EOF_TOKEN:
+        return "EOF_TOKEN";
+    case SEMICOLON:
+        return "SEMICOLON";
+    case COMMA:
+        return "COMMA";
+    default:
+        return "UNKNOWN";
+    }
 }
 
 std::string get_lexer_output(const std::vector<Token> &tokens)
 {
-  std::string output;
-  output += "[\n";
-  for (const Token &token : tokens) {
-    std::string tokenTypeName = tokenTypeToString(token.first);
-    output += "    ('" + tokenTypeName + "', '" + token.second + "'),\n";
-    if (token.first == SEMICOLON) {
-      output += "\n";
+    std::string output;
+    output += "[\n";
+    for (const Token &token : tokens) {
+        std::string tokenTypeName = tokenTypeToString(token.first);
+        output += "    ('" + tokenTypeName + "', '" + token.second + "'),\n";
+        if (token.first == SEMICOLON) {
+            output += "\n";
+        }
     }
-  }
-  output += "]";
-  return output;
+    output += "]";
+    return output;
 }
 
 /*
