efa1acce01358cf25d459f5d4699cea43e95df28
eztaah <eztaah@gmail.com>
2024-03-03 18:59:23 +0100

    fix

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 rm -rf build/*
 mkdir -p build
-make && ./compiler test.cp --output -v build/
+make && ./compiler test.cp -v
--- test_cpp.sh
+++ test_cpp.sh
@@ -2,6 +2,6 @@
 
 # rm -rf build/*
 mkdir -p build
-make && ./compiler test.cp --cpp --output -v build/
+make && ./compiler test.cp -v --cpp --output build/
 g++ -o build/output build/output.cpp
 ./build/output
