abd79f8d576341f9a86f524b8c37b54fa806e2c5
eztaah <eztaah@gmail.com>
2024-03-04 19:27:03 +0100

    semantic analysis + handle functions names errors

---
+++ src/semantic_analysis.cpp
@@ -0,0 +1,35 @@
+#include "semantic_analysis.hpp"
+#include <stdexcept>
+#include <vector>
+
+void visitNode(const NodePtr& node);
+void visitFunctionCall(const FunctionCallNode* node);
+
+void analyzeAST(const NodePtr& node) {
+    visitNode(node);
+}
+
+void visitNode(const NodePtr& node) {
+    if (auto fnode = dynamic_cast<const FunctionCallNode*>(node.get())) {
+        visitFunctionCall(fnode);
+    }
+
+    else if (auto inode = dynamic_cast<const IfNode*>(node.get())) {
+        visitNode(inode->_condition);
+        for (const auto& stmt : inode->_true_block) {
+            visitNode(stmt);
+        }
+        for (const auto& stmt : inode->_false_block) {
+            visitNode(stmt);
+        }
+    }
+}
+
+void visitFunctionCall(const FunctionCallNode* node) {
+    if (node->_name != "println") {
+        throw std::runtime_error("Fonction non autorisée appelée: " + node->_name);
+    }
+    for (const auto& arg : node->_args) {
+        visitNode(arg);
+    }
+}
---
+++ src/semantic_analysis.hpp
@@ -0,0 +1,8 @@
+#ifndef SEMANTIC_ANALYSIS_HPP
+#define SEMANTIC_ANALYSIS_HPP
+
+#include "nodes.hpp"
+
+void analyzeAST(const NodePtr& node);
+
+#endif // SEMANTIC_ANALYSIS_HPP
