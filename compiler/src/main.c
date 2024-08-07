947ec36fad17320e4d7171c36c9366c396ae93f1
eztaah <eztaah@gmail.com>
2024-08-07 17:33:18 +0200

    add license, changelog, and remove -o option

--- compiler/src/main.c
+++ compiler/src/main.c
@@ -7,7 +7,6 @@
 
 Bool dev_mode = FALSE;
 Bool compile = FALSE;
-static Char* output_path = "out/prog";
 static Char* source_path;
 
 static Void print_usage(Void)
@@ -17,9 +16,7 @@ static Void print_usage(Void)
     printf("    compiler --version              display compiler version and exit.\n");
     printf("    compiler --help                 display this help message and exit.\n");
     printf("\nOptions:\n");
-    printf("    -o, --output-dir <directory>    specify the output directory. (default: ./out/)\n");
     printf("    -d, --dev-mode                  activate dev mode\n");
-    printf("\n");
     printf("    -e, --to-executable             assemble and link the generated assembly code into an executable\n");
     printf("                                        - GNU assembler (as) and GNU linker (ld) will be needed during compilation time.\n");
     printf("                                        - the generated executable will only run on x86_64 architecture and requires a Linux system with the GNU C Library (glibc).\n");
@@ -47,17 +44,7 @@ static Void handle_arguments(const I32 argc, Char* argv[])
 
     // handle optional arguments
     for (I32 i = 2; i < argc; i++) {
-        if (char_cmp(argv[i], "-o") || char_cmp(argv[i], "--output")) {
-            if (i + 1 < argc) {
-                output_path = argv[i + 1];
-                i++;
-            }
-            else {
-                printf("-o or --output-dir argument used, but no directory specified");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else if (char_cmp(argv[i], "-d") || char_cmp(argv[i], "--dev-mode")) {
+        if (char_cmp(argv[i], "-d") || char_cmp(argv[i], "--dev-mode")) {
             dev_mode = TRUE;
         }
         else if (char_cmp(argv[i], "-e") || char_cmp(argv[i], "--to-executable")) {
@@ -78,6 +65,7 @@ static Void compile_file(const Char* filename)
 
     // Lexing
     List* token_list = lex(src);
+    free(src);
     Str* token_list_printable = print_tokenlist(token_list);
     write_file("out/lexer_output.txt", str_to_char(token_list_printable));
     str_free(token_list_printable);
@@ -95,8 +83,7 @@ static Void compile_file(const Char* filename)
     write_file("out/generator_output.s", str_to_char(asm_code));
     str_free(asm_code);
 
-    free(src);
-
+    // Assemble and link
     if (compile) {
         sh("as -D out/generator_output.s -o out/prog.o");
         sh("ld -s -o out/prog out/prog.o");
